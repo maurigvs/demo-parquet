02579b17b71817369fbf70827bb477ee7c3769ad
Mauri Gonçalves <mauri.goncalves@accountfy.com>
2023-02-17 10:41:48 -0300

    initial version

---
+++ src/main/java/br/com/maurigvs/demoparquet/DemoParquetApplication.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.demoparquet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParquetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoParquetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/maurigvs/demoparquet/DemoParquetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.demoparquet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParquetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
